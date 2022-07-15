f17c16b89e84c37db00a18e754adde226fc614a3
kyshek904@gmail.com <kyshek904@gmail.com>
2022-07-16 00:25:11 +0800

    u

--- setlocalvar.sh
+++ setlocalvar.sh
@@ -1,2 +1,2 @@
-k8smasterip=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_1')
-k8snode2ip=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_2')
+export MASTERIP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_1')
+export NODE2IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_2')
