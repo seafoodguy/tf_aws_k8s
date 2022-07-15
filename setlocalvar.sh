602c3b2c100d42d2c3eac5f382f01aa121b7203c
kyshek904@gmail.com <kyshek904@gmail.com>
2022-07-16 00:48:45 +0800

    u

--- setlocalvar.sh
+++ setlocalvar.sh
@@ -1,2 +1,8 @@
 export MASTERIP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_1')
 export NODE2IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_2')
+export NODE3IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_3')
+export NODE4IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_4')
+export NODE5IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_5')
+export RANCHERPORT=$(ssh -i ../tf_sshkey/id_rsa k8suser@$MASTERIP kubectl get svc rancher -n cattle-system -o=json |jq -r .spec.ports[1].nodePort)
+RANCHERWEB=$(echo https://$MASTERIP:$RANCHERPORT)
+echo $RANCHERWEB
