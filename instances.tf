1ec45d9cb9095f9c65c96119e2dc17925b02ae2a
kyshek904@gmail.com <kyshek904@gmail.com>
2022-07-15 18:34:48 +0800

    u

--- instances.tf
+++ instances.tf
@@ -59,7 +59,7 @@ resource "aws_security_group" "aws_sandbox_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "tcp"
-    security_groups = [aws_security_group.aws_sandbox_sg.id]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
