228dc145a5f63738e692e5078ea8ccadacfc7cbe
kyshek904@gmail.com <kyshek904@gmail.com>
2022-07-16 16:05:45 +0800

    u

--- setlocalvar.sh
+++ setlocalvar.sh
@@ -3,7 +3,7 @@ export NODE2IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_san
 export NODE3IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_3')
 export NODE4IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_4')
 export NODE5IP=$(terraform output -json aws_sandbox-Public-IPs | jq -r '.aws_sandbox_tf_5')
-export RANCHERPORT=$(ssh -i ../tf_sshkey/id_rsa k8suser@$MASTERIP kubectl get svc rancher -n cattle-system -o=json |jq -r .spec.ports[1].nodePort)
+export RANCHERPORT=$(ssh -i ~/tf_sshkey/id_rsa k8suser@$MASTERIP kubectl get svc rancher -n cattle-system -o=json |jq -r .spec.ports[1].nodePort)
 RANCHERWEB=$(echo https://$MASTERIP:$RANCHERPORT)
 echo "######################################################"
 echo "Rancher web:"
@@ -12,5 +12,5 @@ echo ""
 echo "below Env are set"
 echo "RANCHERWEB, MASTERIP, NODE2IP, NODE3IP, NODE4IP, NODE5IP "
 echo "You can SSH to master node by:"
-echo "ssh -i ../tf_sshkey/id_rsa k8suser@$MASTERIP"
+echo "ssh -i ~/tf_sshkey/id_rsa k8suser@$MASTERIP"
 echo "######################################################"
